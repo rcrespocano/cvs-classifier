e39ff1792cf017f8cb2de530b87601e5461f9e90
Rubén Crespo-Cano <rcrespocano@gmail.com>
2019-03-22 17:55:06 +0100

    Plot AUC (mean and STD) of mean ROC

--- cvs/plot.py
+++ cvs/plot.py
@@ -12,16 +12,18 @@ def plot_roc(roc_data, classifiers_ids):
         for executions in x:
             plt.plot(executions[0], executions[1], alpha=0.10, color='gray')
         
-        plt.legend(loc='lower right')
         plt.plot([0,1], [0,1], 'k--', lw=1)
         plt.ylabel('True Positive Rate')
         plt.xlabel('False Positive Rate')
 
-        # Mean
+        # Mean ROC and AUC
         try:
-            _mean = np.mean(x, axis=0)
-            plt.plot(_mean[0], _mean[1], alpha=0.99, color='blue')
+            _mean_roc = np.mean(x, axis=0)
+            _auc = [item[3] for item in x]
+            _label = r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (np.mean(_auc), np.std(_auc))
+            plt.plot(_mean_roc[0], _mean_roc[1], alpha=0.99, color='blue', label=_label)
         except Exception as exc:
             print(exc)
 
+        plt.legend(loc='lower right')
         plt.show()
