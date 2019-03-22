Fix error: surround mean calculation with try/except block

cvs/plot.py
@@ -10,14 +10,18 @@ def plot_roc(roc_data, classifiers_ids):
         plt.title('Receiver Operating Characteristic - ' + _name)
         
         for executions in x:
-            plt.plot(executions[0], executions[1], alpha=0.2)
+            plt.plot(executions[0], executions[1], alpha=0.10, color='gray')
         
         plt.legend(loc='lower right')
         plt.plot([0,1], [0,1], 'k--', lw=1)
         plt.ylabel('True Positive Rate')
         plt.xlabel('False Positive Rate')
 
-        # Mean and STD
-        _mean = np.mean(x, axis=0)
-        plt.plot(_mean[0], _mean[1], alpha=0.95, color='blue')
+        # Mean
+        try:
+            _mean = np.mean(x, axis=0)
+            plt.plot(_mean[0], _mean[1], alpha=0.99, color='blue')
+        except Exception as exc:
+            print(exc)
+
         plt.show()